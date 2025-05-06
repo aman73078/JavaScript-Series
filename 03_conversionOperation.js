32b2f56f507df53cc18a7937a3c6f46ebd2807d7
Aman Tripathi <aman.tripathi@stellaripl.com>
2025-05-06 09:01:39 +0530

    Part-3: Conversion Operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,90 @@
+// Data Type Conversion Operations
+
+let score = 33;
+let score1 = '33';
+let score2 = "33abc";
+let score3 = null;
+let score4 = undefined;
+let score5 = -1/0;
+let score6 = true;
+let score7 = 'Rahul';
+
+let valueInNumber1 = Number(score1);
+let valueInNumber2 = Number(score2);
+let valueInNumber3 = Number(score3);
+let valueInNumber4 = Number(score4);
+let valueInNumber5 = Number(score5);
+let valueInNumber6 = Number(score6);
+let valueInNumber7 = Number(score7);
+
+let valueInString1 = String(score1);
+let valueInString2 = String(score2);
+let valueInString3 = String(score3);
+let valueInString4 = String(score4);
+let valueInString5 = String(score5);
+let valueInString6 = String(score6);
+let valueInString7 = String(score7);
+
+console.log(`Data Type of score :- ${typeof score} and value is ${score}`);
+console.log(`Data Type of score1 :- ${typeof score1} and value is ${score1}`);
+console.log(`Data Type of score2 :- ${typeof score2} and value is ${score2}`);
+console.log(`Data Type of score3 :- ${typeof score3} and value is ${score3}`);
+console.log(`Data Type of score4 :- ${typeof score4} and value is ${score4}`);
+console.log(`Data Type of score5 :- ${typeof score5} and value is ${score5}`);
+console.log(`Data Type of score6 :- ${typeof score6} and value is ${score6}`);
+console.log(`Data Type of score7 :- ${typeof score7} and value is ${score7}\n\n`);
+
+console.log(`Data Type of valueInNumber1 :- ${typeof valueInNumber1} and value is ${valueInNumber1}`);
+console.log(`Data Type of valueInNumber2 :- ${typeof valueInNumber2} and value is ${valueInNumber2}`);
+console.log(`Data Type of valueInNumber3 :- ${typeof valueInNumber3} and value is ${valueInNumber3}`);
+console.log(`Data Type of valueInNumber4 :- ${typeof valueInNumber4} and value is ${valueInNumber4}`);
+console.log(`Data Type of valueInNumber5 :- ${typeof valueInNumber5} and value is ${valueInNumber5}`);
+console.log(`Data Type of valueInNumber6 :- ${typeof valueInNumber6} and value is ${valueInNumber6}`);
+console.log(`Data Type of valueInNumber7 :- ${typeof valueInNumber7} and value is ${valueInNumber7}\n\n`);
+
+console.log(`Data Type of valueInString1 :- ${typeof valueInString1} and value is ${valueInString1}`);
+console.log(`Data Type of valueInString2 :- ${typeof valueInString2} and value is ${valueInString2}`);
+console.log(`Data Type of valueInString3 :- ${typeof valueInString3} and value is ${valueInString3}`);
+console.log(`Data Type of valueInString4 :- ${typeof valueInString4} and value is ${valueInString4}`);
+console.log(`Data Type of valueInString5 :- ${typeof valueInString5} and value is ${valueInString5}`);
+console.log(`Data Type of valueInString6 :- ${typeof valueInString6} and value is ${valueInString6}`);
+console.log(`Data Type of valueInString7 :- ${typeof valueInString7} and value is ${valueInString7}\n\n`);
+
+// Boolean Conversion
+
+let score8 = 1;
+let score9 = 0;
+let score10 = "";
+let score11 = "hitesh";
+let score12 = true;
+let score13 = false;
+
+let valueInBoolean1 = Boolean(score);
+let valueInBoolean2 = Boolean(score1);
+let valueInBoolean3 = Boolean(score2);
+let valueInBoolean4 = Boolean(score3);
+let valueInBoolean5 = Boolean(score4);
+let valueInBoolean6 = Boolean(score5);
+let valueInBoolean7 = Boolean(score6);
+let valueInBoolean8 = Boolean(score7);
+let valueInBoolean9 = Boolean(score8);
+let valueInBoolean10 = Boolean(score9);
+let valueInBoolean11 = Boolean(score10);
+let valueInBoolean12 = Boolean(score11);
+let valueInBoolean13 = Boolean(score12);
+let valueInBoolean14 = Boolean(score13);
+
+console.log(`Data Type of valueInBoolean1 :- ${typeof valueInBoolean1} and value is ${valueInBoolean1}`);
+console.log(`Data Type of valueInBoolean2 :- ${typeof valueInBoolean2} and value is ${valueInBoolean2}`);
+console.log(`Data Type of valueInBoolean3 :- ${typeof valueInBoolean3} and value is ${valueInBoolean3}`);
+console.log(`Data Type of valueInBoolean4 :- ${typeof valueInBoolean4} and value is ${valueInBoolean4}`);
+console.log(`Data Type of valueInBoolean5 :- ${typeof valueInBoolean5} and value is ${valueInBoolean5}`);
+console.log(`Data Type of valueInBoolean6 :- ${typeof valueInBoolean6} and value is ${valueInBoolean6}`);
+console.log(`Data Type of valueInBoolean7 :- ${typeof valueInBoolean7} and value is ${valueInBoolean7}`);
+console.log(`Data Type of valueInBoolean8 :- ${typeof valueInBoolean8} and value is ${valueInBoolean8}`);
+console.log(`Data Type of valueInBoolean9 :- ${typeof valueInBoolean9} and value is ${valueInBoolean9}`);
+console.log(`Data Type of valueInBoolean10 :- ${typeof valueInBoolean10} and value is ${valueInBoolean10}`);
+console.log(`Data Type of valueInBoolean11:- ${typeof valueInBoolean11} and value is ${valueInBoolean11}`);
+console.log(`Data Type of valueInBoolean12 :- ${typeof valueInBoolean12} and value is ${valueInBoolean12}`);
+console.log(`Data Type of valueInBoolean13 :- ${typeof valueInBoolean13} and value is ${valueInBoolean13}`);
+console.log(`Data Type of valueInBoolean14 :- ${typeof valueInBoolean14} and value is ${valueInBoolean14}`);
