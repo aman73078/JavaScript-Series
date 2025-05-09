e17ac813ad02703effe7b0c87ebdd39cb7fdeb8f
Aman Tripathi <aman.tripathi@stellaripl.com>
2025-05-09 09:48:26 +0530

    updated series

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let name = "hitesh";
+let age = 25;
+let isMarried = false;
+let salary = null;
+let designation;
+console.log(`name : ${typeof name}`);
+console.log(`age : ${typeof age}`);
+console.log(`isMarried : ${typeof isMarried}`);
+console.log(`salary : ${typeof salary}`);
+console.log(`designation : ${typeof designation}`);
+ 
+// Note:- Range of number data type is -(2^53-1) to (2^53-1).
+// Note:- null is representational of empty value. null is a standalone value.
+// Note:- unique
