51964587d9fd78971c042abca694941bde525570
Aman Tripathi <123200498+aman73078@users.noreply.github.com>
2025-05-26 23:32:28 +0530

    object completed

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -6,6 +6,8 @@ userInfo.name = 'Rahul';
 userInfo.age = 32;
 console.log(`Line::01-----> ${JSON.stringify(userInfo)}`);
 
+console.log(`Line::01::A-----> ${JSON.stringify(userInfo)}`);
+
 const address = {
     cityName: 'Lucknow',
     country: 'India',
@@ -49,3 +51,59 @@ console.log(`Line::05----> ${JSON.stringify(Object.values(newUser))}`);
 // Object.entries() :- It returns an array of a given object's own enumerable string-keyed property
 //                      [key, value] pairs.
 console.log(`Line::05----> ${JSON.stringify(Object.entries(newUser))}`);
+
+// It returns boolean value if the property exist in the given object.
+console.log(`Line::06----> ${newUser.hasOwnProperty('isLogged')}`);
+
+// Destructuring
+const myObject = {
+    name:'Rahul',
+    email:'rahul@gmail.com',
+    courseInstructur:'Ravi'
+};
+const {courseInstructur} = myObject;
+console.log(`Line::07----> ${courseInstructur}`);
+
+const {courseInstructur:instructor} = myObject;
+console.log(`Line::08----> ${instructor}`);
+
+const {name,email:gmail,courseInstructur:mentor} = myObject;
+console.log(`Line::09----> ${name}, ${gmail} & ${mentor}`);
+
+/* JSON API :- A JSON API typically refers to an API (Application Programming Interface) that  
+                communicates using JSON (JavaScript Object Notation) as the data format. It enables systems (like web clients and servers) to exchange structured data in a lightweight, readable way. */
+
+{
+    result:[
+        {
+            "id":1,
+            "name":'rahul', 
+            "email":"rahul@gmail.com", 
+            "password":"rahul@1234"
+        },
+        {
+            "id":2, 
+            "name":'kishor', 
+            "email":"kishor@gmail.com", 
+            "password":"kishor@1234"
+        },
+        {
+            "id":3, 
+            "name":'manoj',
+            "email":"manoj@gmail.com", 
+            "password":"manoj@1234"
+        },
+        {
+            "id":4, 
+            "name":'rubi', 
+            "email":"rubi@gmail.com", 
+            "password":"rubi@1234"
+        },
+        {
+            "id":5, 
+            "name":'sonia',
+            "email":"sonia@gmail.com", 
+            "password":"sonia@1234"
+        },
+    ]
+}
